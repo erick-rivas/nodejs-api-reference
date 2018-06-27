626981087d3bed8c274953af462119bf3e1cdd24
Erick Rivas <erick_rivas@outlook.com>
2018-06-27 12:53:03 -0500

    :octocat: Add .gitignore

--- dist/src/models/Mocks.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Mocks = /** @class */ (function () {
-    function Mocks() {
-    }
-    return Mocks;
-}());
-exports["default"] = Mocks;
-//# sourceMappingURL=Mocks.js.map
--- dist/src/models/Model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Model = /** @class */ (function () {
-    function Model() {
-    }
-    return Model;
-}());
-exports["default"] = Model;
-//# sourceMappingURL=Model.js.map
